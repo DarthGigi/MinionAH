6c94c77c8a445037097b027a0e8283295355c565
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-07-10 19:37:26 +0200

    Add rate limits

--- src/routes/api/loadMinions/+server.ts
+++ src/routes/api/loadMinions/+server.ts
@@ -1,6 +1,7 @@
 import type { RequestHandler } from "./$types";
 import type { Minion } from "$lib/types";
 import prisma from "$lib/server/prisma";
+import { dev } from "$app/environment";
 
 type Params = {
   take?: number;
@@ -9,6 +10,14 @@ type Params = {
   distinct?: any;
   where?: any;
 };
+
+const MAX_REQUESTS_PER_MINUTE = 30;
+
+const requestCounts = new Map<string, number>();
+
+setInterval(() => {
+  requestCounts.clear();
+}, 60 * 1000);
 
 async function getMinions(take: number = 9, skip?: number, orderBy: object = [{ generator: "asc" }, { generator_tier: "asc" }], distinct?: any, where?: any) {
   let minions = await prisma.minion.findMany({
@@ -26,7 +35,25 @@ export const POST: RequestHandler = async ({ request }) => {
   // Switch from GET to POST
   const params: Params = await request.json();
 
-  console.log(params);
+  const ip = !dev ? request.headers.get("CF-Connecting-IP") || request.headers.get("X-Forwarded-For") || request.headers.get("X-Real-IP") || request.headers.get("X-Client-IP") || request.headers.get("X-Cluster-Client-IP") || request.headers.get("X-Forwarded") || request.headers.get("Forwarded-For") || request.headers.get("Forwarded") : "127.0.0.1";
+
+  if (!ip) {
+    console.log("No IP address found");
+    return new Response(null, {
+      status: 400
+    });
+  }
+
+  const requestCount = requestCounts.get(ip) || 0;
+
+  if (requestCount >= MAX_REQUESTS_PER_MINUTE) {
+    console.log("Too many requests");
+    return new Response(null, {
+      status: 429
+    });
+  }
+
+  requestCounts.set(ip, requestCount + 1);
 
   let minions;
   try {
