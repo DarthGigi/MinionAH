fb725213437a0f03ec31d5d1b0714ca4a7be1775
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-07-20 17:33:59 +0200

    Update skeleton loader

--- src/lib/components/CardLoading.svelte
+++ src/lib/components/CardLoading.svelte
@@ -1,24 +1,28 @@
-<li class="col-span-1 divide-y divide-neutral-700 rounded-lg bg-neutral-800 shadow">
-  <div class="flex w-full items-center justify-between space-x-6 p-6">
-    <div class="flex-1">
-      <div class="flex items-center justify-start space-x-3">
-        <div class="h-4 w-32 animate-pulse rounded bg-neutral-400" />
-        <div class="inline-flex h-4 w-12 flex-shrink-0 animate-pulse rounded-full bg-neutral-400" />
+<li>
+  <div class="relative col-span-1 list-item divide-y divide-neutral-700 rounded-lg bg-neutral-800">
+    <div class="flex h-full w-full items-center justify-center space-x-6 px-4">
+      <div class="my-2 flex flex-col items-center rounded p-1">
+        <div class="h-12 w-12 flex-shrink-0 overflow-hidden rounded-full bg-neutral-700 p-1 animate-pulse"></div>
+        <span class="w-14 h-5 mt-1 bg-neutral-700 animate-pulse rounded"></span>
+      </div>
+      <div class="my-2 flex flex-col items-center rounded p-1">
+        <div class="h-12 w-12 flex-shrink-0 overflow-hidden rounded-full bg-neutral-700 p-1 animate-pulse"></div>
+        <span class="w-14 h-5 mt-1 bg-neutral-700 animate-pulse rounded"></span>
       </div>
     </div>
-    <div class="h-10 w-10 flex-shrink-0 animate-pulse rounded-full bg-neutral-700 p-1" />
-  </div>
-  <div>
+
     <div class="-mt-px flex divide-x divide-neutral-700">
-      <div class="flex w-0 flex-1 items-center justify-center py-4">
-        <div class="h-5 w-5 animate-pulse rounded bg-neutral-400" />
-        <div class="ml-3 h-4 w-14 animate-pulse rounded bg-neutral-400" />
+      <div class="relative inline-flex w-0 flex-1 items-center justify-center overflow-hidden rounded-bl-lg">
+        <div class="absolute animate-pulse h-5 w-20 flex-shrink-0 rounded-[50px] bg-neutral-700" />
       </div>
-      <div class="-ml-px flex w-0 flex-1">
-        <div class="flex w-0 flex-1 items-center justify-center py-4">
-          <div class="h-5 w-5 animate-pulse rounded bg-neutral-400" />
-          <div class="ml-3 h-4 w-14 animate-pulse rounded bg-neutral-400" />
-        </div>
+      <div class="relative -ml-px inline-flex w-0 flex-1 overflow-hidden">
+        <span class="relative z-10 inline-flex w-0 flex-1 items-center justify-center overflow-hidden py-4">
+          <div class="h-5 w-20 animate-pulse bg-neutral-700 rounded" />
+        </span>
+        <div class="absolute z-0 h-0 w-full flex-shrink-0 bg-neutral-400" />
+      </div>
+      <div class="relative inline-flex w-0 flex-1 items-center justify-center overflow-hidden rounded-br-lg">
+        <div class="absolute animate-pulse h-5 w-20 flex-shrink-0 rounded-[50px] bg-neutral-700" />
       </div>
     </div>
   </div>
