af79c72d82e940d8fce26e8a31473a0f64515c73
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-11-11 15:54:47 +0100

    Add navigation transition effect globally

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
   import { dev } from "$app/environment";
+  import { onNavigate } from "$app/navigation";
   import Navbar from "$lib/components/Navbar.svelte";
   import { inject } from "@vercel/analytics";
   import "../app.css";
   inject({ mode: dev ? "development" : "production", debug: false });
+
+  onNavigate((navigation) => {
+    //@ts-expect-error
+    if (!document.startViewTransition) return;
+
+    return new Promise((resolve) => {
+      //@ts-expect-error
+      document.startViewTransition(async () => {
+        resolve();
+        await navigation.complete;
+      });
+    });
+  });
 </script>
 
 <Navbar />
