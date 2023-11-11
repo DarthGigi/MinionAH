f12d063f7dce1f0f4cd72354c46869dea37ff259
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-11-12 00:34:15 +0100

    Refactor Navbar component styles for mobile menu

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -41,9 +41,9 @@
             </a>
           </div>
         </div>
-        <div class="flex gap-4 items-center">
-          <div class="flex-shrink-0 hidden md:block">
-            <a href="https://minions.mrgigi.me/discord" class="relative inline-flex items-center rounded-md border border-transparent shadow-sm bg-neutral-600 focus:outline-none focus:ring-2 focus:ring-neutral-500 focus:ring-offset-2 focus:ring-offset-neutral-800 flex-row justify-center hover:bg-[#5865F2] px-4 py-2 text-sm font-medium text-white transition-colors duration-300">
+        <div class="flex items-center gap-4">
+          <div class="hidden flex-shrink-0 md:block">
+            <a href="https://minions.mrgigi.me/discord" class="relative inline-flex flex-row items-center justify-center rounded-md border border-transparent bg-neutral-600 px-4 py-2 text-sm font-medium text-white shadow-sm transition-colors duration-300 hover:bg-[#5865F2] focus:outline-none focus:ring-2 focus:ring-neutral-500 focus:ring-offset-2 focus:ring-offset-neutral-800">
               <!-- Heroicon name: mini/plus -->
               <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-5 w-5" fill="currentColor" viewBox="0 0 24 24"><path d="M19.3034 5.33716C17.9344 4.71103 16.4805 4.2547 14.9629 4C14.7719 4.32899 14.5596 4.77471 14.411 5.12492C12.7969 4.89144 11.1944 4.89144 9.60255 5.12492C9.45397 4.77471 9.2311 4.32899 9.05068 4C7.52251 4.2547 6.06861 4.71103 4.70915 5.33716C1.96053 9.39111 1.21766 13.3495 1.5891 17.2549C3.41443 18.5815 5.17612 19.388 6.90701 19.9187C7.33151 19.3456 7.71356 18.73 8.04255 18.0827C7.41641 17.8492 6.82211 17.5627 6.24904 17.2231C6.39762 17.117 6.5462 17.0003 6.68416 16.8835C10.1438 18.4648 13.8911 18.4648 17.3082 16.8835C17.4568 17.0003 17.5948 17.117 17.7434 17.2231C17.1703 17.5627 16.576 17.8492 15.9499 18.0827C16.2789 18.73 16.6609 19.3456 17.0854 19.9187C18.8152 19.388 20.5875 18.5815 22.4033 17.2549C22.8596 12.7341 21.6806 8.80747 19.3034 5.33716ZM8.5201 14.8459C7.48007 14.8459 6.63107 13.9014 6.63107 12.7447C6.63107 11.5879 7.45884 10.6434 8.5201 10.6434C9.57071 10.6434 10.4303 11.5879 10.4091 12.7447C10.4091 13.9014 9.57071 14.8459 8.5201 14.8459ZM15.4936 14.8459C14.4535 14.8459 13.6034 13.9014 13.6034 12.7447C13.6034 11.5879 14.4323 10.6434 15.4936 10.6434C16.5442 10.6434 17.4038 11.5879 17.3825 12.7447C17.3825 13.9014 16.5548 14.8459 15.4936 14.8459Z" /></svg>
               <span>Community</span>
@@ -100,7 +100,15 @@
         <Transition show={isMobileMenuOpen} class="border-t border-neutral-700 pb-3 pt-4" enter="transition duration-300" enterFrom="transform opacity-0 scale-95" enterTo="transform scale-100 opacity-100" leave="transition duration-300" leaveFrom="transform scale-100 opacity-100 " leaveTo="transform opacity-0 scale-95">
           <div class="flex items-center px-5 sm:px-6">
             <div class="flex-shrink-0">
-              <img class="pointer-events-none h-10 w-10 rounded-full" src={` https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}?size=64`} alt="" />
+              <img
+                class="pointer-events-none h-10 w-10 rounded-full"
+                src={` https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}?size=64`}
+                alt={user.username}
+                on:error={({ currentTarget }) => {
+                  if (!(currentTarget instanceof HTMLImageElement)) return;
+                  currentTarget.src = `https://cdn.discordapp.com/embed/avatars/${Number(user.id) % 6}.png?size=64`;
+                }}
+              />
             </div>
             <div class="ml-3">
               <div class="text-base font-medium text-white">{user.username}</div>
