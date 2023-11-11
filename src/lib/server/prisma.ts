214573c75e493e52a6330199411884cac1a04c98
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-11-11 16:44:10 +0100

    Add Prisma extension for query acceleration.

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,10 +1,11 @@
 import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 declare global {
   var prisma: PrismaClient;
 }
 
-const prisma = global.prisma || new PrismaClient({ log: ["info"] });
+const prisma = global.prisma || new PrismaClient({ log: ["info"] }).$extends(withAccelerate());
 global.prisma = prisma;
 
 export default prisma;
