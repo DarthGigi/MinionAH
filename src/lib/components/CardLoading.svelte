a72f74747d3fe49c374e3c9af790a35c4663ece2
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-11-12 00:34:02 +0100

    Refactor CardLoading component styles.

--- src/lib/components/CardLoading.svelte
+++ src/lib/components/CardLoading.svelte
@@ -2,27 +2,27 @@
   <div class="relative col-span-1 list-item divide-y divide-neutral-700 rounded-lg bg-neutral-800">
     <div class="flex h-full w-full items-center justify-center space-x-6 px-4">
       <div class="my-2 flex flex-col items-center rounded p-1">
-        <div class="h-12 w-12 flex-shrink-0 overflow-hidden rounded-full bg-neutral-700 p-1 animate-pulse"></div>
-        <span class="w-14 h-5 mt-1 bg-neutral-700 animate-pulse rounded"></span>
+        <div class="h-12 w-12 flex-shrink-0 animate-pulse overflow-hidden rounded-full bg-neutral-700 p-1"></div>
+        <span class="mt-1 h-5 w-14 animate-pulse rounded bg-neutral-700"></span>
       </div>
       <div class="my-2 flex flex-col items-center rounded p-1">
-        <div class="h-12 w-12 flex-shrink-0 overflow-hidden rounded-full bg-neutral-700 p-1 animate-pulse"></div>
-        <span class="w-14 h-5 mt-1 bg-neutral-700 animate-pulse rounded"></span>
+        <div class="h-12 w-12 flex-shrink-0 animate-pulse overflow-hidden rounded-full bg-neutral-700 p-1"></div>
+        <span class="mt-1 h-5 w-14 animate-pulse rounded bg-neutral-700"></span>
       </div>
     </div>
 
     <div class="-mt-px flex divide-x divide-neutral-700">
       <div class="relative inline-flex w-0 flex-1 items-center justify-center overflow-hidden rounded-bl-lg">
-        <div class="absolute animate-pulse h-5 w-20 flex-shrink-0 rounded-[50px] bg-neutral-700" />
+        <div class="absolute h-5 w-20 flex-shrink-0 animate-pulse rounded-[50px] bg-neutral-700" />
       </div>
       <div class="relative -ml-px inline-flex w-0 flex-1 overflow-hidden">
         <span class="relative z-10 inline-flex w-0 flex-1 items-center justify-center overflow-hidden py-4">
-          <div class="h-5 w-20 animate-pulse bg-neutral-700 rounded" />
+          <div class="h-5 w-20 animate-pulse rounded bg-neutral-700" />
         </span>
         <div class="absolute z-0 h-0 w-full flex-shrink-0 bg-neutral-400" />
       </div>
       <div class="relative inline-flex w-0 flex-1 items-center justify-center overflow-hidden rounded-br-lg">
-        <div class="absolute animate-pulse h-5 w-20 flex-shrink-0 rounded-[50px] bg-neutral-700" />
+        <div class="absolute h-5 w-20 flex-shrink-0 animate-pulse rounded-[50px] bg-neutral-700" />
       </div>
     </div>
   </div>
