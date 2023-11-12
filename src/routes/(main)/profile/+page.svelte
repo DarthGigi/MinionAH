0b8b44c69ebf96afb9b4de6a1af31544f7f90678
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-11-12 01:35:40 +0100

    Refactor profile page layout and styling

--- src/routes/(main)/profile/+page.svelte
+++ src/routes/(main)/profile/+page.svelte
@@ -22,11 +22,11 @@
 <div class="mx-auto flex max-w-xl flex-col justify-center gap-8 self-center">
   <div class="w-full pt-8">
     <h3 class="text-lg font-medium leading-6 text-neutral-200">Logged in as</h3>
-    <dl class="mt-5 relative">
+    <dl class="relative mt-5">
       <button
         id="minionButton"
         data-dropdown-toggle="dropdown"
-        class="absolute right-3 top-3 text-neutral-400 group bg-neutral-700 focus:ring-4 focus:outline-none focus:ring-transparent rounded-lg text-sm p-1.5"
+        class="group absolute right-3 top-3 rounded-lg bg-neutral-700 p-1.5 text-sm text-neutral-400 focus:outline-none focus:ring-4 focus:ring-transparent"
         type="button"
         on:click={() => {
           // copy url to clipboard
@@ -40,11 +40,11 @@
         }}
       >
         {#if !copied}
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="group-hover:text-white transition-colors duration-300 w-5 h-5">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 transition-colors duration-300 group-hover:text-white">
             <path stroke-linecap="round" stroke-linejoin="round" d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z" />
           </svg>
         {:else}
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 text-green-400 h-5">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 text-green-400">
             <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
         {/if}
@@ -52,7 +52,15 @@
       <div class="group w-full rounded-lg border-2 border-neutral-700 border-opacity-40 bg-[#050505] bg-cover bg-center bg-no-repeat px-4 py-5 shadow sm:p-6" style="background-image: url('https://cdn.discordapp.com/banners/{user.id}/{user.banner}?size=1024'); background-color: {user.accent_color ? '#' + user.accent_color : '#050505'};">
         <dt class="hidden truncate text-sm font-medium text-neutral-400">Profile</dt>
         <dd class="mt-1 text-3xl font-semibold tracking-tight text-neutral-300">
-          <img src="https://cdn.discordapp.com/avatars/{user.id}/{user.avatar}?size=1024" class="inline-block h-10 w-10 rounded-full" alt="User Avatar" />
+          <img
+            src="https://cdn.discordapp.com/avatars/{user.id}/{user.avatar}?size=1024"
+            class="inline-block h-10 w-10 rounded-full"
+            alt={user.username}
+            on:error={({ currentTarget }) => {
+              if (!(currentTarget instanceof HTMLImageElement)) return;
+              currentTarget.src = `https://cdn.discordapp.com/embed/avatars/${Number(user.id) % 6}.png?size=64`;
+            }}
+          />
           <br />
           {user.username}
           <br />
@@ -82,11 +90,11 @@
                 <div class="mt-1 flex flex-col rounded-md shadow-sm">
                   <TierListbox />
                 </div>
-                <div class="mt-1 flex flex-col rounded-md justify-center shadow-sm">
+                <div class="mt-1 flex flex-col justify-center rounded-md shadow-sm">
                   <!-- checkbox -->
                   <div class="flex h-5 items-center">
-                    <input id="infusion" name="infusion" type="checkbox" class="h-4 w-4 rounded border-gray-300 focus:checked:bg-neutral-700 hover:checked:bg-neutral-700 checked:bg-neutral-600 ring-0 focus:ring-neutral-700" />
-                    <label for="infusion" class="font-medium ml-1 text-neutral-200">Mithril Infused</label>
+                    <input id="infusion" name="infusion" type="checkbox" class="h-4 w-4 rounded border-gray-300 ring-0 checked:bg-neutral-600 hover:checked:bg-neutral-700 focus:ring-neutral-700 focus:checked:bg-neutral-700" />
+                    <label for="infusion" class="ml-1 font-medium text-neutral-200">Mithril Infused</label>
                   </div>
                 </div>
               </div>
@@ -167,7 +175,7 @@
       {:then minions}
         {#each minions as seller}
           <Card
-            {seller}
+            minion={seller}
             on:openDeleteModal={({ detail }) => {
               showDelete = true;
               minionToDelete = detail.minion;
