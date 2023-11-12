dd9060d6677cd42e0c5de6d3e4a2eabca8ca2800
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-11-12 18:45:49 +0100

    Add transition animation and loading spinner to minion cards

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -6,6 +6,7 @@
   import TierListbox from "$lib/components/TierListbox.svelte";
   import { Input } from "$lib/components/ui/input";
   import { Label } from "$lib/components/ui/label";
+  import { draw } from "svelte/transition";
 
   export let data: PageData;
   let minions: Seller[] = [];
@@ -13,12 +14,14 @@
   let currentTier: number | undefined = undefined;
   let newMinionAmount: number;
   let initialDispatch = true;
+  let initialLoad = true;
 
   let search: string | undefined = undefined;
 
   (async function () {
     minions = await data.props.minions;
     loadingMore = false;
+    initialLoad = false;
   })();
 
   async function loadData(filterTier?: number | undefined, skip?: number, search?: string, isMore: boolean = false) {
@@ -104,12 +107,16 @@
 <div class="py-8">
   <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
     <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-      {#await minions then minions}
+      {#await minions}
+        {#each Array(9) as _}
+          <CardLoading />
+        {/each}
+      {:then minions}
         {#each minions as seller}
           <Card minion={seller} />
         {/each}
 
-        {#if loadingMore}
+        {#if loadingMore && initialLoad}
           {#each Array(9) as _}
             <CardLoading />
           {/each}
@@ -120,12 +127,14 @@
       <!-- check if there are no longer any items in the database -->
       {#if newMinionAmount === 0 || newMinionAmount < 9 || (minions.length === 0 && !loadingMore)}
         <p class="px-4 py-1 text-center text-sm text-neutral-200 text-opacity-40">No more minions to load.</p>
-      {:else if loadingMore}
-        <p class="px-4 py-1 text-center text-sm text-neutral-200 text-opacity-60">Loading...</p>
       {:else}
         <button type="button" on:click={() => loadData(currentTier, minions.length, search, true)} class="rounded p-1 text-sm text-white text-opacity-30 transition-all duration-300 hover:bg-neutral-700 hover:text-opacity-100">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6" class:animate-spin={loadingMore}>
+            {#if loadingMore}
+              <path in:draw={{ duration: 500, delay: 500 }} out:draw={{ duration: 500 }} stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+            {:else}
+              <path in:draw={{ duration: 500, delay: 500 }} out:draw={{ duration: 500 }} stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+            {/if}
           </svg>
         </button>
       {/if}
