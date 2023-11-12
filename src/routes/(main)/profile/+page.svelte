7b2b641ba596d9a234301f7b40172e9828cdba78
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-11-12 13:48:48 +0100

    Refactor code for improved readability and performance.

--- src/routes/(main)/profile/+page.svelte
+++ src/routes/(main)/profile/+page.svelte
@@ -1,19 +1,25 @@
 <script lang="ts">
   import { enhance } from "$app/forms";
   import { page } from "$app/stores";
-  import Card from "$lib/components/Card.svelte";
+  import MinionCard from "$lib/components/Card.svelte";
   import CardLoading from "$lib/components/CardLoading.svelte";
+  import MinionCopyButton from "$lib/components/MinionCopyButton.svelte";
   import MinionsListBox from "$lib/components/MinionsListBox.svelte";
   import TierListbox from "$lib/components/TierListbox.svelte";
+  import * as Card from "$lib/components/ui/card";
+  import { Input } from "$lib/components/ui/input";
+  import { Label } from "$lib/components/ui/label";
+  import { Switch } from "$lib/components/ui/switch";
   import type { User } from "@prisma/client";
   import { Dialog, DialogOverlay, DialogTitle, Transition, TransitionChild } from "@rgossiaux/svelte-headlessui";
   import type { PageData } from "./$types";
 
   let isOpen = false;
-  let copied = false;
 
   export let data: PageData;
   $: user = data.user as User;
+
+  let moreThan1 = false;
 
   let showDelete = false;
   let minionToDelete: string | null = null;
@@ -23,32 +29,8 @@
   <div class="w-full pt-8">
     <h3 class="text-lg font-medium leading-6 text-neutral-200">Logged in as</h3>
     <dl class="relative mt-5">
-      <button
-        id="minionButton"
-        data-dropdown-toggle="dropdown"
-        class="group absolute right-3 top-3 rounded-lg bg-neutral-700 p-1.5 text-sm text-neutral-400 focus:outline-none focus:ring-4 focus:ring-transparent"
-        type="button"
-        on:click={() => {
-          // copy url to clipboard
-          navigator.clipboard.writeText(`${window.location.protocol}//${window.location.host}/${user.username}/`);
-          // change the icon to a checkmark
-          copied = true;
-          // change the icon back to a minion icon after 2 seconds
-          setTimeout(() => {
-            copied = false;
-          }, 2000);
-        }}
-      >
-        {#if !copied}
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 transition-colors duration-300 group-hover:text-white">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z" />
-          </svg>
-        {:else}
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 text-green-400">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-          </svg>
-        {/if}
-      </button>
+      <MinionCopyButton class="absolute right-3 top-3" on:click={() => navigator.clipboard.writeText(`${window.location.protocol}/${window.location.host}/${user.username}/`)} />
+
       <div class="group w-full rounded-lg border-2 border-neutral-700 border-opacity-40 bg-[#050505] bg-cover bg-center bg-no-repeat px-4 py-5 shadow sm:p-6" style="background-image: url('https://cdn.discordapp.com/banners/{user.id}/{user.banner}?size=1024'); background-color: {user.accent_color ? '#' + user.accent_color : '#050505'};">
         <dt class="hidden truncate text-sm font-medium text-neutral-400">Profile</dt>
         <dd class="mt-1 text-3xl font-semibold tracking-tight text-neutral-300">
@@ -71,67 +53,71 @@
   </div>
   {#await data.streamed.userMinions then userMinions}
     {#if userMinions.length < 9}
-      <form id="createForm" use:enhance method="POST" action="?/createMinion" class="space-y-8 divide-y divide-neutral-800 rounded-lg border-2 border-neutral-700 border-opacity-40 bg-[#050505] px-6 py-8">
-        <div class="space-y-8 divide-y divide-neutral-800">
-          <div>
-            <div>
-              <h3 class="text-lg font-medium leading-6 text-neutral-200">Minions</h3>
-              <p class="mt-1 text-sm text-neutral-400">Put a minion in the AH</p>
-            </div>
-
-            <div class="mt-6">
-              <div class="flex gap-4 sm:col-span-4">
-                <div class="mt-1 flex flex-col rounded-md shadow-sm">
+      <Card.Root class="border-2 border-neutral-700 border-opacity-40 bg-[#050505] text-neutral-200">
+        <form id="createForm" use:enhance method="POST" action="?/createMinion">
+          <Card.Header>
+            <Card.Title>Minions</Card.Title>
+            <Card.Description>Auction a minion</Card.Description>
+          </Card.Header>
+          <Card.Content>
+            <div class="grid w-full items-center justify-center gap-4">
+              <div class="flex gap-4">
+                <div>
                   {#await data.streamed.minionTypes then minionType}
                     <MinionsListBox {minionType} />
                   {/await}
                 </div>
-                <div class="mt-1 flex flex-col rounded-md shadow-sm">
+                <div>
                   <TierListbox />
-                </div>
-                <div class="mt-1 flex flex-col justify-center rounded-md shadow-sm">
-                  <!-- checkbox -->
-                  <div class="flex h-5 items-center">
-                    <input id="infusion" name="infusion" type="checkbox" class="h-4 w-4 rounded border-gray-300 ring-0 checked:bg-neutral-600 hover:checked:bg-neutral-700 focus:ring-neutral-700 focus:checked:bg-neutral-700" />
-                    <label for="infusion" class="ml-1 font-medium text-neutral-200">Mithril Infused</label>
-                  </div>
                 </div>
               </div>
 
-              <div class="flex gap-4 sm:col-span-4">
-                <div class="mt-1 flex flex-col rounded-md shadow-sm">
-                  <label for="amount" class="block font-medium text-neutral-200">Amount of minions</label>
-                  <input
+              <!-- checkbox -->
+
+              <div class="flex gap-4">
+                <div class="mt-1 inline-flex flex-col rounded-md shadow-sm">
+                  <Label for="amount" class="block text-base font-normal">Amount of minions</Label>
+                  <Input
                     type="number"
                     name="amount"
                     id="amount"
                     max="64"
                     min="1"
-                    class="block h-9 w-full rounded-md border-neutral-300 bg-black placeholder-neutral-500 shadow-sm focus:border-neutral-500 focus:ring-neutral-500 sm:text-sm"
+                    class="w-44 ring-offset-0 focus-visible:border-neutral-500 focus-visible:ring-1 focus-visible:ring-neutral-500 focus-visible:ring-offset-0"
                     placeholder="1"
-                    on:input={(e) => {
-                      // @ts-ignore
-                      if (e.target.valueAsNumber > 64) {
-                        // @ts-ignore
-                        e.target.value = "64";
+                    on:input={({ currentTarget }) => {
+                      if (!(currentTarget instanceof HTMLInputElement)) return;
+                      if (currentTarget.valueAsNumber > 64) {
+                        currentTarget.value = "64";
                       }
-                      // @ts-ignore
-                      if (e.target.valueAsNumber < 1) {
-                        // @ts-ignore
-                        e.target.value = "1";
+                      if (currentTarget.valueAsNumber < 1) {
+                        currentTarget.value = "1";
                       }
+                      if (currentTarget.valueAsNumber > 1) {
+                        moreThan1 = true;
+                      } else {
+                        moreThan1 = false;
+                      }
+                    }}
+                    on:keydown={(e) => {
+                      if (e.key === "e" || e.key === "." || e.key === "-" || e.key === "+" || e.key === "E" || e.key === " " || e.key === ",") {
+                        e.preventDefault();
+                      }
+                    }}
+                    on:paste={(e) => {
+                      e.preventDefault();
                     }}
                   />
                 </div>
-                <div class="mt-1 flex flex-col rounded-md shadow-sm">
-                  <label for="price" class="block font-medium text-neutral-200">Price <span class="text-xs text-neutral-200/50">(each)</span></label>
-                  <input
+                <div class="mt-1 inline-flex flex-col rounded-md shadow-sm">
+                  <Label for="price" class="block text-base font-normal">Price <span class="text-xs text-neutral-200/50 opacity-0 transition-opacity duration-500" class:opacity-100={moreThan1}>(each)</span></Label>
+                  <Input
                     type="number"
-                    min="1"
-                    max="10000000000"
                     name="price"
                     id="price"
-                    class="block h-9 w-full rounded-md border-neutral-300 bg-black placeholder-neutral-500 shadow-sm invalid:border-red-500 focus:border-neutral-500 focus:ring-neutral-500 sm:text-sm"
+                    min="1"
+                    max="10000000000"
+                    class="w-44 ring-offset-0 focus-visible:border-neutral-500 focus-visible:ring-1 focus-visible:ring-neutral-500 focus-visible:ring-offset-0"
                     placeholder="100"
                     on:keydown={(e) => {
                       if (e.key === "e" || e.key === "." || e.key === "-" || e.key === "+" || e.key === "E" || e.key === " " || e.key === ",") {
@@ -144,16 +130,19 @@
                   />
                 </div>
               </div>
+              <div>
+                <div class="inline-flex flex-col">
+                  <Label for="infusion" class="inline-flex text-base font-normal">Mithril Infused</Label>
+                  <Switch id="infusion" name="infusion" class="data-[state=checked]:bg-neutral-800 data-[state=unchecked]:bg-neutral-700" />
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-
-        <div class="pt-5">
-          <div class="flex justify-end">
+          </Card.Content>
+          <Card.Footer class="justify-end">
             <button type="submit" class="ml-3 inline-flex justify-center rounded-md border border-transparent bg-neutral-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-neutral-700 focus:outline-none focus:ring-2 focus:ring-neutral-500 focus:ring-offset-2">Create</button>
-          </div>
-        </div>
-      </form>
+          </Card.Footer>
+        </form>
+      </Card.Root>
     {:else}
       <div class="space-y-8 divide-y divide-neutral-800 rounded-lg border-2 border-neutral-700 border-opacity-40 bg-[#050505] px-6 py-8">
         <div class="space-y-8 divide-y divide-neutral-800">
@@ -173,7 +162,7 @@
         {/each}
       {:then minions}
         {#each minions as seller}
-          <Card
+          <MinionCard
             minion={seller}
             on:openDeleteModal={({ detail }) => {
               showDelete = true;
