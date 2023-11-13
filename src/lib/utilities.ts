98fb7f8dd06e6f0fb263f475da77ca555feee560
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-11-13 13:40:54 +0100

    Add support for string input in formatNumber function

--- src/lib/utilities.ts
+++ src/lib/utilities.ts
@@ -1,6 +1,11 @@
 import { format } from "numerable";
 
-export function formatNumber(num: number): string {
+export function formatNumber(num: number | string): string {
+  // if the number is a string, parse it to a number
+  if (typeof num === "string") {
+    num = parseInt(num);
+  }
+
   // check the last digits, if the last 2 or 3 digits are 0, don't format
   const lastDigits = num % 1000;
   if (lastDigits === 0 || lastDigits === 0o0 || lastDigits === 0o00) {
