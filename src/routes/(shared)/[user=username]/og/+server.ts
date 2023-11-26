2dbe901bc05d39b7230b1ceede54f8f6356ed07e
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-11-26 14:41:03 +0100

    Refactor user avatar rendering in og/+server.ts

--- src/routes/(shared)/[user=username]/og/+server.ts
+++ src/routes/(shared)/[user=username]/og/+server.ts
@@ -77,21 +77,14 @@ export const GET: RequestHandler = async ({ params, fetch }) => {
     }
   }
 
-  let avatarUrl;
-  const avatar = await fetch(`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=1024`);
-  if (avatar.status === 404) {
-    avatarUrl = `https://cdn.discordapp.com/embed/avatars/${Number(user.id) % 6}.png?size=1024`;
-  } else {
-    avatarUrl = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=1024`;
-  }
-
   const template = toReactElement(`
   <div tw="flex h-full w-full flex-col items-center justify-center bg-[#131313]">
     <div tw="flex w-full max-w-2xl justify-center rounded-lg border border-neutral-700 bg-neutral-800 shadow">
       <div tw="mx-auto flex flex-col items-center rounded py-20">
-        <img tw="mb-3 h-44 w-44 rounded-full shadow-lg" src="${avatarUrl}" />
-        <span tw="mb-1 text-4xl font-medium text-white">@${user.username}</span>
-        <span tw="text-2xl text-neutral-400">${user.id}</span>
+        <div tw="flex mb-3 items-center h-44 w-44 shadow-lg overflow-hidden justify-center rounded-full bg-neutral-700">
+          <img tw="h-full w-full p-4" src="data:image/png;base64,${user.avatar}" />
+        </div>
+        <span tw="mb-1 text-4xl font-medium text-white">${user.username}</span>
       </div>
     </div>
   </div>
