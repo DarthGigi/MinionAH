cd82d293423e61ff1d24b565588de064c8899555
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-12-02 16:24:48 +0100

    Refactor formatNumber function to handle different number formats

--- src/lib/utilities.ts
+++ src/lib/utilities.ts
@@ -3,16 +3,24 @@ import { format } from "numerable";
 export function formatNumber(num: number | string): string {
   // if the number is a string, parse it to a number
   if (typeof num === "string") {
-    num = parseInt(num);
+    num = parseFloat(num);
   }
-
-  // check the last digits, if the last 2 or 3 digits are 0, don't format
-  const lastDigits = num % 1000;
-  if (lastDigits === 0 || lastDigits === 0o0 || lastDigits === 0o00) {
+  // get the second digit
+  const secondDigit = num.toString().charAt(1);
+  // get the third digit
+  const thirdDigit = num.toString().charAt(2);
+  let formatPattern: string;
+  if (secondDigit === "0" && thirdDigit === "0") {
+    formatPattern = "0a";
+  } else if (secondDigit !== "0" && thirdDigit === "0") {
+    formatPattern = "0.0a";
+  } else {
+    formatPattern = "0.00a";
+  }
+  // if number is in trillions, billions, millions, thousands, format it to 1 decimal place if the decimal is 0, otherwise format it to 0 decimal places
+  if (num >= 1000000) {
+    return format(num, formatPattern);
+  } else {
     return format(num, "0a");
   }
-  // if there are only 1, 2 or 3 digits, don't format
-  else if (num < 1000) {
-    return format(num, "0a");
-  } else return format(num, "0.00a");
 }
