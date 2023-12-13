13131467fa9789ac4daa8f2c7458e620535d2152
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-12-13 18:58:22 +0100

    Update vite.config.ts with new plugin name

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-import { vitePluginSvelteKitOG } from "@ethercorps/svelte-h2j/vite";
+import { vitePluginSvelteH2J } from "@ethercorps/svelte-h2j/vite";
 
 export default defineConfig({
-  plugins: [sveltekit(), vitePluginSvelteKitOG()]
+  plugins: [sveltekit(), vitePluginSvelteH2J()]
 });
