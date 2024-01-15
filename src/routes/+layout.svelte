d235ff5306fb08a5425631d556eb8fd717d075e6
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-01-15 22:24:00 +0100

    Add preference handling for Minecraft font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
   import { dev } from "$app/environment";
   import Navbar from "$lib/layouts/Navbar.svelte";
+  import { preferences } from "$lib/stores/preferences";
   import { inject } from "@vercel/analytics";
   import "../app.css";
   inject({ mode: dev ? "development" : "production", debug: false });
+
+  preferences.subscribe((value) => {
+    if (typeof window !== "undefined") {
+      if (value.minecraftFont) {
+        document.body.classList.add("font-minecraft");
+      } else {
+        document.body.classList.remove("font-minecraft");
+      }
+    }
+  });
 </script>
 
 <Navbar />
