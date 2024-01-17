e88ed03cd1c2f0e447ed439eb73bd6b670db832c
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-01-17 23:21:20 +0100

    Refactor chat components and add slide transition

--- src/lib/components/chat/chat-date.svelte
+++ src/lib/components/chat/chat-date.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+  import { slide } from "svelte/transition";
+
   export let date: Date;
+  export let self: boolean;
   function formatDate(createdAt: Date) {
     const messageDate = createdAt;
     const today = new Date();
@@ -20,4 +23,4 @@
   }
 </script>
 
-<li class="text-center text-xs text-neutral-500">{formatDate(date)}</li>
+<div transition:slide|global={{ axis: "x" }} class="self-center text-center text-xs text-neutral-500" class:ml-2={self} class:mr-2={!self}>{formatDate(date)}</div>
