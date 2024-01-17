2f9ff953334e7bdcf83c61202d24e2966c7329ea
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-01-17 21:07:06 +0100

    Make chat components for displaying date and loading animations

---
+++ src/lib/components/chat/chat-date.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  export let date: Date;
+  function formatDate(createdAt: Date) {
+    const messageDate = createdAt;
+    const today = new Date();
+    const yesterday = new Date(today);
+    yesterday.setDate(today.getDate() - 1);
+
+    if (isSameDay(messageDate, today)) {
+      return "Today";
+    } else if (isSameDay(messageDate, yesterday)) {
+      return "Yesterday";
+    } else {
+      return messageDate.toLocaleDateString();
+    }
+  }
+
+  function isSameDay(date1: Date, date2: Date) {
+    return date1.getDate() === date2.getDate() && date1.getMonth() === date2.getMonth() && date1.getFullYear() === date2.getFullYear();
+  }
+</script>
+
+<li class="text-center text-xs text-neutral-500">{formatDate(date)}</li>
---
+++ src/lib/components/chat/chat-loading.svelte
@@ -0,0 +1,15 @@
+<li class="animate-pulse self-end rounded-full rounded-br-none bg-[#3C83F7] px-4 py-2">
+  <div class="h-4 w-16" />
+</li>
+<li class="animate-pulse self-start rounded-full rounded-bl-none bg-[#3B3B3D] px-4 py-2">
+  <div class="h-4 w-16" />
+</li>
+<li class="animate-pulse self-end rounded-full rounded-br-none bg-[#3C83F7] px-4 py-2">
+  <div class="h-4 w-16" />
+</li>
+<li class="animate-pulse self-start rounded-full rounded-bl-none bg-[#3B3B3D] px-4 py-2">
+  <div class="h-4 w-16" />
+</li>
+<li class="animate-pulse self-end rounded-full rounded-br-none bg-[#3C83F7] px-4 py-2">
+  <div class="h-4 w-16" />
+</li>
