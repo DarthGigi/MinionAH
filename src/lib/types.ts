0a731c135936e37cb7a65c10f93e2fd921da2b16
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-01-31 15:45:53 +0100

    Add id and timeCreated properties to Seller interface

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,6 +2,8 @@ import type { Minion, MinionSeller, User } from "@prisma/client";
 
 // extend the MinionSeller type to include the user and minion
 interface Seller extends MinionSeller {
+  id: any;
+  timeCreated: string | number | Date;
   user: User;
   minion: Minion;
 }
