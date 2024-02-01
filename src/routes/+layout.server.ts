a28eb46d57098692396863b6aaff03c010281245
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-02-01 01:27:15 +0100

    Add isAdmin property to layout server load

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,6 +2,7 @@ import type { LayoutServerLoad } from "./$types";
 
 export const load = (async ({ locals }) => {
   return {
-    [locals.user !== null ? "user" : ""]: locals.user
+    [locals.user !== null ? "user" : ""]: locals.user,
+    [locals.isAdmin ? "isAdmin" : ""]: locals.isAdmin
   };
 }) satisfies LayoutServerLoad;
