ccba40453e39630bee4a076098a30bbfb90c1235
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-02-02 18:34:17 +0100

    Refactor minion loading in dashboard page

--- src/routes/(main)/(protected)/dashboard/minions/+page.server.ts
+++ src/routes/(main)/(protected)/dashboard/minions/+page.server.ts
@@ -1,31 +1,16 @@
 import type { PageServerLoad } from "./$types";
 
 export const load = (async () => {
-  const minionsCount = await prisma.minion.count();
-  const [minionsBatch1, minionsBatch2] = await Promise.all([
-    prisma.minion.findMany({
-      take: minionsCount / 2,
-      include: {
-        _count: {
-          select: {
-            sellers: true
-          }
+  const minions = await prisma.minion.findMany({
+    include: {
+      _count: {
+        select: {
+          sellers: true
         }
       }
-    }),
-    prisma.minion.findMany({
-      skip: minionsCount / 2,
-      include: {
-        _count: {
-          select: {
-            sellers: true
-          }
-        }
-      }
-    })
-  ]);
+    }
+  });
   return {
-    minionsBatch1,
-    minionsBatch2
+    minions
   };
 }) satisfies PageServerLoad;
--- src/routes/(main)/(protected)/dashboard/minions/+page.svelte
+++ src/routes/(main)/(protected)/dashboard/minions/+page.svelte
@@ -2,8 +2,6 @@
   import type { PageData } from "./$types";
   import DataTable from "./data-table.svelte";
   export let data: PageData;
-
-  const minions = [...data.minionsBatch1, ...data.minionsBatch2];
 </script>
 
-<DataTable data={minions} />
+<DataTable data={data.minions} />
