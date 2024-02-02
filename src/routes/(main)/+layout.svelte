61ffe8300723f1e19bfd4206eabdf58a03ded3a1
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-02-02 19:36:15 +0100

    Update page metadata and descriptions

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -1,17 +1,17 @@
 <svelte:head>
   <!-- Primary Meta Tags -->
-  <title>MinionAH — The Auction House for Hypixel SkyBlock Minions</title>
-  <meta name="title" content="MinionAH — The Auction House for Hypixel SkyBlock Minions" />
-  <meta name="description" content="Buy and sell Hypixel SkyBlock minions easily on MinionAH. Explore a seamless marketplace with a beautiful interface for minion trading." />
-  <meta name="theme-color" content="#131313" />
+  <title>MinionAH - The Auction House for SkyBlock Minions</title>
+  <meta name="title" content="MinionAH: The Auction House for SkyBlock Minions" />
+  <meta name="description" content="MinionAH is the best place to trade Hypixel SkyBlock minions. Minion auction house has a modern design, has QOL features for trading skyblock minions, and more." />
+  <meta name="theme-color" content="#171717" />
   <link rel="canonical" href="https://minionah.com" />
-  <meta name="keywords" content="MinionAH, Hypixel SkyBlock, minions, auction house, minion trading, skyblock auction, skyblock minions, Hypixel minions, buy minions, sell minions" />
+  <meta name="keywords" content="MinionAH, Hypixel SkyBlock, minions, auction house, minion trading, skyblock auction, skyblock minions, Hypixel minions, buy minions, sell minions, minionah, skyblock, minion ah, minion auctions, minion skyblock" />
 
   <!-- Open Graph / Facebook -->
   <meta property="og:type" content="website" />
   <meta property="og:url" content="https://minionah.com" />
-  <meta property="og:title" content="MinionAH — The Auction House for Hypixel SkyBlock Minions" />
-  <meta property="og:description" content="Buy and sell Hypixel SkyBlock minions easily on MinionAH. Explore a seamless marketplace with a beautiful interface for minion trading." />
+  <meta property="og:title" content="MinionAH: The Auction House for SkyBlock Minions" />
+  <meta property="og:description" content="MinionAH is the best place to trade Hypixel SkyBlock minions. Minion auction house has a modern design, has QOL features for trading skyblock minions, and more." />
   <meta property="og:image" content="https://minionah.com/assets/images/ogBanner.png?v=2" />
   <meta property="og:image:secure_url" content="https://minionah.com/assets/images/ogBanner.png?v=2" />
   <meta property="og:image:alt" content="MinionAH" />
@@ -24,8 +24,8 @@
   <!-- Twitter -->
   <meta property="twitter:card" content="summary_large_image" />
   <meta property="twitter:url" content="https://minionah.com" />
-  <meta property="twitter:title" content="MinionAH" />
-  <meta property="twitter:description" content="Buy and sell Hypixel SkyBlock minions easily on MinionAH. Explore a seamless marketplace with a beautiful interface for minion trading." />
+  <meta property="twitter:title" content="MinionAH: The Auction House for SkyBlock Minions" />
+  <meta property="twitter:description" content="MinionAH is the best place to trade Hypixel SkyBlock minions. Minion auction house has a modern design, has QOL features for trading skyblock minions, and more." />
   <meta property="twitter:image" content="https://minionah.com/assets/images/ogBanner.png?v=2" />
 </svelte:head>
 <slot />
