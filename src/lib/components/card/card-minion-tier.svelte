1b5f3fe5652ed3f8e2602ff8ab2ee408804476e3
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-02-10 21:58:41 +0100

    Refactor card component styles

--- src/lib/components/card/card-minion-tier.svelte
+++ src/lib/components/card/card-minion-tier.svelte
@@ -3,9 +3,10 @@
   import type { Seller } from "$lib/types";
   import { getContext } from "svelte";
   const minion = getContext<Seller>("minion");
+  const isHome = getContext<boolean>("isHome");
 </script>
 
-<div class="relative inline-flex w-0 flex-1 items-center justify-center overflow-hidden rounded-bl-lg text-sm font-medium text-primary">
+<div class="relative inline-flex w-0 flex-1 items-center justify-center overflow-hidden text-sm font-medium text-primary" class:rounded-bl-lg={!isHome}>
   <span class="z-10 inline-block flex-shrink-0 rounded-full px-2 py-0.5 text-xs font-medium text-secondary transition-transform duration-300 group-hover:scale-125 group-hover:text-muted">
     {#if $preferences.romanNumerals}
       {` Tier ${["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII"][minion.minion.generator_tier - 1]}`}
