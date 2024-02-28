9565729f77b8104cf5dc137da407b86cf4abb1d3
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-02-28 22:26:10 +0100

    Refactor TierListbox component to make it generic instead of a form

--- src/lib/components/TierListbox.svelte
+++ src/lib/components/TierListbox.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
   import { page } from "$app/stores";
-  import * as Form from "$lib/components/ui/form";
+  import * as Select from "$lib/components/ui/select";
   import { preferences } from "$lib/stores/preferences";
   import { createEventDispatcher } from "svelte";
 
-  const dispatch = createEventDispatcher();
+  const dispatch = createEventDispatcher<{ onSelectedTierChange: { tier: number | undefined | unknown } }>();
 
   export let maxtier: number = 12;
-  export let config: any;
   export let disabled: boolean = false;
-  export let showValidation: boolean = true;
 
   let selected = false;
 
@@ -23,31 +21,25 @@
   let showAny = false;
 </script>
 
-<Form.Field {config} name="tier">
-  <Form.Item class="flex flex-col">
-    <Form.Label>Tier</Form.Label>
-    <Form.Select
-      {disabled}
-      onSelectedChange={(v) => {
-        selected = true;
-        dispatch("filterTier", { tier: v ? v.value : undefined });
-        if ($page.url.pathname !== "/profile" && v?.value !== 0) showAny = true;
-      }}>
-      <Form.SelectTrigger placeholder="Select tier" class={`w-40 border-none bg-accent font-medium focus:border-accent focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-0 md:w-44 ${!selected ? "text-muted-foreground" : ""}`} aria-label="Select tier"></Form.SelectTrigger>
-      <Form.SelectContent class="scrollbar max-h-56 overflow-y-auto overflow-x-clip border border-border bg-popover text-popover-foreground">
-        {#if showAny}
-          <Form.SelectItem label="Any" value={0} class="px-2 data-[highlighted]:bg-background">Any</Form.SelectItem>
-        {/if}
-
-        {#each tiers as tier}
-          <Form.SelectItem label={`${$preferences.romanNumerals ? ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII"][tier - 1] : tier} `} value={tier} class="px-2 data-[highlighted]:bg-background">
-            {`${$preferences.romanNumerals ? ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII"][tier - 1] : tier}`}
-          </Form.SelectItem>
-        {/each}
-      </Form.SelectContent>
-    </Form.Select>
-    {#if showValidation}
-      <Form.Validation class="w-40 md:w-44" />
+<Select.Root
+  {disabled}
+  onSelectedChange={(v) => {
+    selected = true;
+    dispatch("onSelectedTierChange", { tier: v ? v.value : undefined });
+    if ($page.url.pathname !== "/profile" && v?.value !== 0) showAny = true;
+  }}>
+  <Select.Trigger class={`w-40 border-none bg-accent font-medium focus:border-accent focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-0 md:w-44 ${!selected ? "text-muted-foreground" : ""}`} aria-label="Select tier">
+    <Select.Value placeholder="Select tier" />
+  </Select.Trigger>
+  <Select.Content class="scrollbar max-h-56 overflow-y-auto overflow-x-clip border border-border bg-popover text-popover-foreground">
+    {#if showAny}
+      <Select.Item label="Any" value={0} class="px-2 data-[highlighted]:bg-background">Any</Select.Item>
     {/if}
-  </Form.Item>
-</Form.Field>
+
+    {#each tiers as tier}
+      <Select.Item label={`${$preferences.romanNumerals ? ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII"][tier - 1] : tier} `} value={tier} class="px-2 data-[highlighted]:bg-background">
+        {`${$preferences.romanNumerals ? ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII"][tier - 1] : tier}`}
+      </Select.Item>
+    {/each}
+  </Select.Content>
+</Select.Root>
