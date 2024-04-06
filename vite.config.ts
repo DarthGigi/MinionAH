84bf62daeb46a5b0b1d442758defe962572988f4
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-04-07 01:26:29 +0200

    Update Sentry configuration and organization name in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [
     sentrySvelteKit({
       sourceMapsUploadOptions: {
-        org: "MinionAH",
+        org: "minionah",
         project: "minionah"
       }
     }),
