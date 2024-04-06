03151226c8804874cf96d0da1620d2f0a2ab56e5
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-04-07 01:19:14 +0200

    Update Sentry configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,15 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [sentrySvelteKit({
-    sourceMapsUploadOptions: {
-      org: "gigi-44",
-      project: "javascript-sveltekit"
-    }
-  }), sveltekit()],
+  plugins: [
+    sentrySvelteKit({
+      sourceMapsUploadOptions: {
+        org: "MinionAH",
+        project: "minionah"
+      }
+    }),
+    sveltekit()
+  ],
   build: {
     sourcemap: true
   }
