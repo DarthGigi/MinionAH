857ded508caaa8f840bee58524cc2cfa0f543eb1
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-04-08 19:04:02 +0200

    Refactor GET color API route to handle error and provide informative status response

--- src/routes/api/internal/color/+server.ts
+++ src/routes/api/internal/color/+server.ts
@@ -2,21 +2,32 @@ import { getAverageColor } from "fast-average-color-node";
 import type { RequestHandler } from "./$types";
 
 export const GET: RequestHandler = async ({ request }) => {
-  const imageUrl = request.headers.get("imageUrl");
-  if (!imageUrl) {
+  try {
+    const imageUrl = request.headers.get("imageUrl");
+    if (!imageUrl) {
+      return new Response("#171717", {
+        status: 400,
+        headers: {
+          "content-type": "text/plain"
+        },
+        statusText: "No imageUrl header"
+      });
+    }
+
+    const color = (await getAverageColor(imageUrl)).hex;
+    return new Response(color, {
+      headers: {
+        "content-type": "text/plain"
+      }
+    });
+  } catch (e) {
+    console.error(e);
     return new Response("#171717", {
-      status: 400,
+      status: 500,
       headers: {
         "content-type": "text/plain"
       },
-      statusText: "No imageUrl header"
+      statusText: "Error getting color"
     });
   }
-
-  const color = (await getAverageColor(imageUrl)).hex;
-  return new Response(color, {
-    headers: {
-      "content-type": "text/plain"
-    }
-  });
 };
