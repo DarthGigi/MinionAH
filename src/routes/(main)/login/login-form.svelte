ee5b905cbb161675a5b486f2c79c2d6220129547
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-04-09 19:07:37 +0200

    Redesign login and signup form

---
+++ src/routes/(main)/login/login-form.svelte
@@ -0,0 +1,109 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button";
+  import * as Card from "$lib/components/ui/card";
+  import * as Form from "$lib/components/ui/form";
+  import { Input } from "$lib/components/ui/input";
+  import LoaderCircle from "lucide-svelte/icons/loader-circle";
+  import { createEventDispatcher } from "svelte";
+  import { toast } from "svelte-sonner";
+  import { writable } from "svelte/store";
+  import { superForm, type Infer, type SuperValidated } from "sveltekit-superforms";
+  import { zodClient } from "sveltekit-superforms/adapters";
+  import { formSchema, type FormSchema } from "./schema";
+
+  export let data: SuperValidated<Infer<FormSchema>>;
+
+  const form = superForm(data, {
+    validators: zodClient(formSchema),
+    dataType: "json",
+    timeoutMs: 2000,
+    validationMethod: "oninput"
+  });
+
+  const { form: formData, enhance, tainted, isTainted, submitting, timeout } = form;
+
+  const toastLoading = writable<number | string>();
+
+  const dispatch = createEventDispatcher();
+
+  const handleSignUpButtonClick = () => {
+    dispatch("signup");
+  };
+
+  timeout.subscribe((value) => {
+    if (value) {
+      toast.loading("It's taking longer than expected to log you in...", {
+        id: $toastLoading
+      });
+    }
+  });
+</script>
+
+<Card.Root class="border-border bg-popover">
+  <Card.Header>
+    <Card.Title>Login</Card.Title>
+    <Card.Description>Enter your MinionAH credentials below to login to your account</Card.Description>
+  </Card.Header>
+  <Card.Content>
+    <form
+      method="POST"
+      use:enhance={{
+        onSubmit: async () => {
+          $toastLoading = toast.loading("Logging you in...");
+        },
+        onResult: async () => {
+          setTimeout(() => toast.dismiss($toastLoading), 300);
+        },
+        onUpdate: async ({ result }) => {
+          if (result.type === "success") {
+            toast.success("Logged in successfully!");
+          } else {
+            toast.error("Failed to login.");
+          }
+        },
+        onError: async () => {
+          toast.error("Something went wrong while trying to login.");
+        }
+      }}
+      class="relative mx-auto flex h-1/2 max-w-md flex-col justify-center self-center px-4 md:px-0">
+      <Form.Field {form} name="username">
+        <Form.Control let:attrs>
+          <Form.Label for="username">Username</Form.Label>
+          <Form.Description>This is your <span class="font-semibold">Minecraft</span> username.</Form.Description>
+          <Input {...attrs} bind:value={$formData.username} maxlength={16} type="text" class="border-2 border-accent transition-all duration-300 focus:border-muted-foreground focus:ring-0 focus-visible:ring-0 focus-visible:ring-offset-0 data-[invalid]:border-destructive/40 focus:data-[invalid]:border-destructive" autocomplete="username" name="username" id="username" />
+          <Form.FieldErrors />
+        </Form.Control>
+      </Form.Field>
+      <Form.Field {form} name="current-password">
+        <Form.Control let:attrs>
+          <Form.Label for="current-password">Password</Form.Label>
+          <Form.Description>
+            This is your <span class="font-semibold">MinionAH</span> password.
+          </Form.Description>
+          <Input {...attrs} bind:value={$formData["current-password"]} type="password" name="current-password" id="current-password" autocomplete="current-password" passwordrules="minlength: 8;" class="border-2 border-accent transition-all duration-300 focus:border-muted-foreground focus:ring-0 focus-visible:ring-0 focus-visible:ring-offset-0 data-[invalid]:border-destructive/40 focus:data-[invalid]:border-destructive" />
+          <Form.FieldErrors />
+        </Form.Control>
+      </Form.Field>
+
+      <Form.Button disabled={!isTainted($tainted) || $submitting} class="transition-all duration-300">
+        {#if !$submitting}
+          Login
+        {:else}
+          <LoaderCircle class="h-4 w-4 animate-spin" />
+        {/if}
+      </Form.Button>
+
+      <span class="my-2 w-full text-center text-sm opacity-50">Or</span>
+      <Button href="/api/oauth" variant="outline" data-disabled={$submitting} data-sveltekit-preload-data="tap" class="data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50">
+        <img src="/assets/images/mc-auth.svg" class="pointer-events-none mr-1 h-6 w-auto select-none transition-opacity duration-300 group-hover:opacity-70" alt="Mc-Auth" />
+        Login with MC-Auth
+      </Button>
+    </form>
+  </Card.Content>
+  <Card.Footer>
+    <p class="w-full text-center text-sm">
+      <span class="opacity-50">Don't have an account?</span>
+      <Button variant="link" on:click={handleSignUpButtonClick} class={`inline-block p-0 underline underline-offset-2 opacity-50 transition-opacity duration-300 hover:opacity-100 ${$submitting ? "pointer-events-none cursor-default" : ""}`}>Sign up</Button>
+    </p>
+  </Card.Footer>
+</Card.Root>
---
+++ src/routes/(main)/login/signup-form.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+  import * as Accordion from "$lib/components/ui/accordion";
+  import { Button } from "$lib/components/ui/button";
+  import * as Card from "$lib/components/ui/card";
+  import * as Collapsible from "$lib/components/ui/collapsible";
+  import ChevronsUpDown from "lucide-svelte/icons/chevrons-up-down";
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  const handleSignInButtonClick = () => {
+    dispatch("signin");
+  };
+</script>
+
+<Card.Root class="border-border bg-popover">
+  <Card.Header>
+    <Card.Title>Sign Up</Card.Title>
+    <Card.Description>Sign up for a an account to get the most out of MinionAH</Card.Description>
+  </Card.Header>
+  <Card.Content>
+    <div class="flex flex-col items-center justify-center">
+      <Button data-sveltekit-preload-data="tap" href="/api/oauth" class="w-full">
+        <img src="/assets/images/mc-auth.svg" class="pointer-events-none mr-1 h-6 w-auto" alt="Mc-Auth" />
+        Sign up with MC-Auth
+      </Button>
+      <span class="mt-2 text-xs text-muted-foreground">This is a one-time process</span>
+    </div>
+  </Card.Content>
+  <Card.Footer>
+    <p class="w-full text-center text-sm">
+      <span class="opacity-50">Already have an account?</span>
+      <Button variant="link" on:click={handleSignInButtonClick} class={`inline-block p-0 underline underline-offset-2 opacity-50 transition-opacity duration-300 hover:opacity-100`}>Sign in</Button>
+    </p>
+  </Card.Footer>
+</Card.Root>
+
+<Collapsible.Root class="mt-6 flex w-full flex-col gap-y-6">
+  <Collapsible.Trigger class="flex w-full items-center justify-between rounded-lg border border-border px-6 py-2 transition-colors duration-300 hover:bg-background ">FAQ <ChevronsUpDown class="size-5" /></Collapsible.Trigger>
+  <Collapsible.Content class="rounded-lg border border-border px-6 py-2">
+    <Accordion.Root class="w-full text-muted-foreground">
+      <Accordion.Item value="item-1" class="border-border">
+        <Accordion.Trigger class="text-accent-foreground">Is it safe?</Accordion.Trigger>
+        <Accordion.Content>
+          Yes. We use MC-Auth to authenticate your Minecraft account, which is the most secure way to do so.
+          <br /><br />
+          No sensitive information like your password, tokens, or any other personal information is being used or stored during this process.
+          <br /><br />
+          For more information, check out <a href="https://mc-auth.com" target="_blank" rel="noopener" class="text-accent-foreground underline">MC-Auth</a>.
+        </Accordion.Content>
+      </Accordion.Item>
+      <Accordion.Item value="item-2" class="border-border">
+        <Accordion.Trigger class="text-accent-foreground">Can I trust you with my account?</Accordion.Trigger>
+        <Accordion.Content>
+          Yes. We will never try to steal, sell, or otherwise misuse your Minecraft account information. We use the information provided by MC-Auth to verify your Minecraft identity.
+          <br /><br />
+          We will also never ask for your Minecraft password or any other sensitive information.
+        </Accordion.Content>
+      </Accordion.Item>
+      <Accordion.Item value="item-3" class="border-border">
+        <Accordion.Trigger class="text-accent-foreground">What information do you store?</Accordion.Trigger>
+        <Accordion.Content>
+          We store the following information:
+          <ul class="list-inside list-disc">
+            <li>Your Minecraft UUID</li>
+            <li>Your Minecraft username</li>
+            <li>Your Minecraft skin</li>
+            <li>Your Minecraft cape (if any)</li>
+          </ul>
+        </Accordion.Content>
+      </Accordion.Item>
+      <Accordion.Item value="item-4" class="border-none">
+        <Accordion.Trigger class="text-accent-foreground">What if your database is compromised?</Accordion.Trigger>
+        <Accordion.Content>In the extremely unlikely event that our database is compromised, your Minecraft account will not be at risk at all and you will not have to change your Minecraft password.</Accordion.Content>
+      </Accordion.Item>
+    </Accordion.Root>
+  </Collapsible.Content>
+</Collapsible.Root>
