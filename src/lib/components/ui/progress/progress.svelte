4a5e7057e9da1ad886d6de4309f58cb2046afa6a
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-04-25 23:09:14 +0200

    Update shadcn-svelte components

--- src/lib/components/ui/progress/progress.svelte
+++ src/lib/components/ui/progress/progress.svelte
@@ -11,5 +11,5 @@
 </script>
 
 <ProgressPrimitive.Root class={cn("relative h-4 w-full overflow-hidden rounded-full bg-secondary", className)} {...$$restProps}>
-  <div class="h-full w-full flex-1 bg-primary transition-all" style={`transform: translateX(-${100 - (100 * (value ?? 0)) / (max ?? 1)}%)`} />
+  <div class="h-full w-full flex-1 bg-primary transition-all" style={`transform: translateX(-${100 - (100 * (value ?? 0)) / (max ?? 1)}%)`}></div>
 </ProgressPrimitive.Root>
