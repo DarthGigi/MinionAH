613f1b2e630f81d6e02f7e26fa9554f7d45ec617
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-07-31 18:16:01 +0200

    feat: Add chat signal to signals store

--- src/lib/stores/signals.ts
+++ src/lib/stores/signals.ts
@@ -1,2 +1,5 @@
+import type { Seller } from "$lib/types";
 import { writable } from "svelte/store";
+
 export const searchSignal = writable("");
+export const chatSignal = writable<Seller | undefined>();
