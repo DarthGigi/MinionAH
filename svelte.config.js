37098c120322f99421b83e467830514a41a1cf78
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-08-20 19:48:06 +0200

    refactor: Update img-src directive in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@ const config = {
       directives: {
         "script-src": ["self", "unsafe-eval", "https://*.vercel-scripts.com", "https://*.vercel.app", "https://*.pusher.com", "sha256-y2WkUILyE4eycy7x+pC0z99aZjTZlWfVwgUAfNc1sY8="],
         "worker-src": ["self", "blob:", "https://*.minionah.com"],
-        "img-src": ["self", "data:", "https://*.vercel.app", "https://*.vercel-scripts.com", "https://*.imgur.com", "https://*.imgbb.com", "https://*.vgy.me", "https://*.gyazo.com", "https://*.prnt.sc", "https://*.prntscr.com", "https://*.tenor.com", "https://*.giphy.com", "https://*.gfycat.com", "https://*.discordapp.net", "https://*.discordapp.com", "https://*.discord.com", "https://*.minionah.com", "https://*.cloudinary.com"],
+        "img-src": ["self", "data:", "https://*.vercel.app", "https://*.vercel-scripts.com", "https://*.imgur.com", "https://*.imgbb.com", "https://*.vgy.me", "https://*.gyazo.com", "https://*.prnt.sc", "https://*.prntscr.com", "https://*.tenor.com", "https://*.giphy.com", "https://*.gfycat.com", "https://*.discordapp.net", "https://*.discordapp.com", "https://*.discord.com", "https://*.minionah.com", "https://*.cloudinary.com", "https://*.hypixel.net"],
         "style-src": ["self", "unsafe-inline"]
       },
       mode: "auto"
