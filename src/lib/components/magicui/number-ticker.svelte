f87a665d1d005a11605e70c01dba0d11255d8ffa
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-08-20 19:47:44 +0200

    feat: Add NumberTicker component

---
+++ src/lib/components/magicui/number-ticker.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { cn } from "$lib/utils";
+  import { onMount } from "svelte";
+  import { cubicOut } from "svelte/easing";
+  import { tweened } from "svelte/motion";
+  export let value = 100;
+  export let initial = 0;
+  export let duration = 6000;
+  let num = tweened(initial, {
+    duration: duration,
+    easing: cubicOut
+  });
+  let className: string = "";
+  export { className as class };
+  onMount(() => {
+    num.set(value);
+  });
+</script>
+
+<div class={cn("inline-block tracking-normal", className)} {...$$restProps}>
+  {$num.toFixed(0)}
+</div>
