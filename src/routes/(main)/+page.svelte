a431c359b24d37fb30af15b7045128e78582414b
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2025-02-19 19:24:13 +0100

    feat: replace AnimatedShinyText with AnimatedGradientText and update button structure

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -2,7 +2,7 @@
   import { MinionCard } from "$lib/components/card";
   import CardLoading from "$lib/components/CardLoading.svelte";
   import HtmlToast from "$lib/components/HtmlToast.svelte";
-  import AnimatedShinyText from "$lib/components/magicui/animated-shiny-text.svelte";
+  import AnimatedGradientText from "$lib/components/magicui/animated-gradient-text.svelte";
   import MinionsListBox from "$lib/components/MinionsListBox.svelte";
   import NumberFlowTicker from "$lib/components/NumberFlowTicker.svelte";
   import SearchSelect, { SearchType } from "$lib/components/SearchSelect.svelte";
@@ -11,11 +11,15 @@
   import * as Card from "$lib/components/ui/card";
   import * as Collapsible from "$lib/components/ui/collapsible";
   import { Label } from "$lib/components/ui/label";
+  import { Separator } from "$lib/components/ui/separator";
   import UsersListBox from "$lib/components/UsersListBox.svelte";
   import { internalPreferences, preferences } from "$lib/stores/preferences";
   import { searchSignal } from "$lib/stores/signals";
   import type { Seller } from "$lib/types";
   import { cn } from "$lib/utils";
+  import ChevronDown from "lucide-svelte/icons/chevron-down";
+  import ChevronUp from "lucide-svelte/icons/chevron-up";
+  import ChevronsUp from "lucide-svelte/icons/chevrons-up";
   import ChevronsUpDown from "lucide-svelte/icons/chevrons-up-down";
   import LayoutGrid from "lucide-svelte/icons/layout-grid";
   import MessagesSquare from "lucide-svelte/icons/messages-square";
@@ -246,21 +250,23 @@
 
 <h2 class="sr-only">MinionAH - The Auction House for SkyBlock Minions</h2>
 
-<div class="mt-8 h-10 md:mt-20 md:h-20">
-  <!-- <GradualSpacing class="text-center text-4xl font-bold tracking-[-0.1em] text-white md:text-7xl md:leading-[5rem]" text="MinionAH" /> -->
-  <div class="flex justify-center space-x-1">
-    <span class="text-center text-4xl font-bold tracking-[-0.045em] text-white md:text-7xl md:leading-[5rem]">MinionAH</span>
-  </div>
-</div>
+<div class="mt-8 flex justify-center space-x-1 text-center text-4xl font-bold tracking-[-0.045em] text-white md:mt-20 md:text-7xl md:leading-[5rem]">MinionAH</div>
 
-<Button href="https://discord.minionah.com" target="_blank" class="group z-10 mx-auto mt-2 flex w-fit items-center justify-center rounded-full border border-white/5 bg-neutral-900 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-800" variant="ghost">
+<!-- <Button href="https://discord.minionah.com" target="_blank" class="group z-10 mx-auto mt-2 flex w-fit items-center justify-center rounded-full border border-white/5 bg-neutral-900 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-800" variant="ghost">
   <AnimatedShinyText class="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-400 hover:duration-300">
     Join our Discord
-    <!-- <ArrowRight class="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" /> -->
     <svg viewBox="0 0 256 199" width="256" height="199" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" class="ml-2 size-4">
       <path d="M216.856 16.597A208.502 208.502 0 0 0 164.042 0c-2.275 4.113-4.933 9.645-6.766 14.046-19.692-2.961-39.203-2.961-58.533 0-1.832-4.4-4.55-9.933-6.846-14.046a207.809 207.809 0 0 0-52.855 16.638C5.618 67.147-3.443 116.4 1.087 164.956c22.169 16.555 43.653 26.612 64.775 33.193A161.094 161.094 0 0 0 79.735 175.3a136.413 136.413 0 0 1-21.846-10.632 108.636 108.636 0 0 0 5.356-4.237c42.122 19.702 87.89 19.702 129.51 0a131.66 131.66 0 0 0 5.355 4.237 136.07 136.07 0 0 1-21.886 10.653c4.006 8.02 8.638 15.67 13.873 22.848 21.142-6.58 42.646-16.637 64.815-33.213 5.316-56.288-9.08-105.09-38.056-148.36ZM85.474 135.095c-12.645 0-23.015-11.805-23.015-26.18s10.149-26.2 23.015-26.2c12.867 0 23.236 11.804 23.015 26.2.02 14.375-10.148 26.18-23.015 26.18Zm85.051 0c-12.645 0-23.014-11.805-23.014-26.18s10.148-26.2 23.014-26.2c12.867 0 23.236 11.804 23.015 26.2 0 14.375-10.148 26.18-23.015 26.18Z" fill="currentColor" />
     </svg>
   </AnimatedShinyText>
+</Button> -->
+
+<Button href="https://newsroom.minionah.com/automatic-deletion/" target="_blank" class="text- group relative z-10 mx-auto mt-2 flex w-fit items-center justify-center rounded-full border-0 border-white/5 bg-neutral-900 px-8 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-800" variant="ghost">
+  <AnimatedGradientText colorClass="from-[#C4B7B2]/50 via-[#574330]/50 to-[#E4E4E4]/50">
+    <ChevronsUp class="mx-0 size-4 text-muted-foreground transition-transform duration-300 ease-in-out group-hover:-translate-y-0.5" />
+    <Separator class="mx-1 h-4 min-h-0" orientation="vertical" decorative={true} />
+    <span class="text-muted-foreground"> Bumping & Automatic Auction Deletion </span>
+  </AnimatedGradientText>
 </Button>
 
 <div class="mx-auto mt-[1.375rem] flex gap-1 px-1 md:max-w-2xl md:gap-2 md:px-2">
@@ -306,12 +312,14 @@
 </div>
 
 <div class="mx-auto my-4 max-w-sm">
-  <Collapsible.Root class="mt-6 flex w-full flex-col gap-y-2">
-    <Collapsible.Trigger class="flex w-full items-center justify-between rounded-lg border border-border px-6 py-2 transition-colors duration-300 hover:bg-background ">
-      Handy Links <ChevronsUpDown class="size-5" />
+  <Collapsible.Root open={true} class="group/root mt-6 flex w-full flex-col gap-y-2">
+    <Collapsible.Trigger class="flex w-full items-center justify-between rounded-lg border border-border px-6 py-2 transition-colors duration-300 hover:bg-background">
+      Handy Links
+      <ChevronUp class="size-5 group-data-[state=closed]/root:hidden" />
+      <ChevronDown class="size-5 group-data-[state=open]/root:hidden" />
     </Collapsible.Trigger>
-    <Collapsible.Content class="rounded-lg border border-border px-6 py-2">
-      <Button href="/pricecheck" variant="link" class="group flex-col items-start justify-start p-0 text-start hover:no-underline">
+    <Collapsible.Content class="rounded-lg border border-border p-0">
+      <Button href="/pricecheck" variant="link" class="group h-full w-full flex-col items-start justify-start px-6 py-3 text-start hover:no-underline">
         <span class="group-hover:underline">Price Checker</span>
         <span class="text-muted-foreground">Check the price of any minion</span>
       </Button>
