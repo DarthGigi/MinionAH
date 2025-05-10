0a05df5efd217cc141bb78e8090031b53693e5c7
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2025-05-10 14:11:32 +0200

    feat: refactor Discord OAuth code to streamline state and code verifier handling

--- src/routes/api/oauth/discord/+server.ts
+++ src/routes/api/oauth/discord/+server.ts
@@ -6,25 +6,26 @@ import { Discord, generateCodeVerifier, generateState } from "arctic";
 const discord = new Discord(DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, dev ? "http://localhost:5173/api/oauth/discord/callback" : DISCORD_REDIRECT_URI);
 
 const state = generateState();
-const url = discord.createAuthorizationURL(state, {
-  scopes: ["identify"]
-});
 const codeVerifier = generateCodeVerifier();
+const scopes = ["identify"];
+const url = discord.createAuthorizationURL(state, codeVerifier, scopes);
 
 export const GET: RequestHandler = async ({ cookies }) => {
   // the state can be stored in cookies or localstorage for request validation on callback
-  cookies.set("discord_code_verifier", codeVerifier, {
+  cookies.set("discord_oauth_state", state, {
+    secure: !dev,
     path: "/",
     httpOnly: true,
-    maxAge: 60 * 60
+    maxAge: 60 * 10 // 10min
   });
 
-  cookies.set("discord_oauth_state", state, {
+  cookies.set("discord_code_verifier", codeVerifier, {
+    secure: !dev,
     path: "/",
     httpOnly: true,
-    maxAge: 60 * 60
+    maxAge: 60 * 10 // 10min
   });
 
   // redirect to authorization url
-  redirect(302, await url);
+  redirect(302, url);
 };
