c82c8fb85480618f74365849ec11ca71703ba5e7
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2025-05-11 16:08:44 +0200

    feat: add account deletion functionality

--- src/routes/(main)/(protected)/profile/(profile)/settings/+page.server.ts
+++ src/routes/(main)/(protected)/profile/(profile)/settings/+page.server.ts
@@ -1,8 +1,18 @@
+import { verify, type Options } from "@node-rs/argon2";
 import { fail, type Actions } from "@sveltejs/kit";
 import { message, superValidate } from "sveltekit-superforms";
 import { zod } from "sveltekit-superforms/adapters";
 import type { PageServerLoad } from "../$types";
+import { deleteAccountFormSchema } from "./delete-account-form.svelte";
 import { profileFormSchema } from "./profile-form.svelte";
+
+const hashOptions = {
+  // recommended minimum parameters
+  memoryCost: 19456,
+  timeCost: 2,
+  outputLen: 32,
+  parallelism: 1
+} satisfies Options;
 
 export const load: PageServerLoad = async ({ locals }) => {
   const [userSettings, discordAccount] = await Promise.all([
@@ -29,7 +39,7 @@ export const load: PageServerLoad = async ({ locals }) => {
   ]);
 
   return {
-    form: await superValidate(
+    updateForm: await superValidate(
       {
         username: locals.user!.username,
         discord: discordAccount?.providerUsername,
@@ -39,12 +49,13 @@ export const load: PageServerLoad = async ({ locals }) => {
       },
       zod(profileFormSchema)
     ),
+    deleteForm: await superValidate(zod(deleteAccountFormSchema)),
     discordAccount
   };
 };
 
 export const actions: Actions = {
-  default: async ({ request, locals }) => {
+  updateProfile: async ({ request, locals }) => {
     const form = await superValidate(request, zod(profileFormSchema));
     if (!form.valid) {
       return fail(400, {
@@ -88,6 +99,56 @@ export const actions: Actions = {
               }
             }
           }
+        }
+      });
+      return { form };
+    } catch (e) {
+      console.error(e);
+      return message(form, "Something went wrong. Please try again.", {
+        status: 500
+      });
+    }
+  },
+  deleteAccount: async ({ request, locals }) => {
+    const form = await superValidate(request, zod(deleteAccountFormSchema));
+
+    if (!form.valid) return fail(400, { form });
+
+    const key = await prisma.key.findFirst({
+      where: {
+        id: `username:${locals.user?.username.toLowerCase()}`
+      }
+    });
+
+    if (!key) {
+      return message(
+        form,
+        { title: "Failed to delete your account", description: "How did you get here?" },
+        {
+          status: 400
+        }
+      );
+    }
+
+    const formPassword = form.data.password;
+    const savedPassword = key.hashed_password!;
+    const validPassword = await verify(savedPassword, formPassword, hashOptions);
+
+    if (!validPassword) {
+      return message(
+        form,
+        { title: "Failed to delete your account", description: "The password you entered is incorrect. Please try again." },
+        {
+          status: 400
+        }
+      );
+    }
+
+    // Delete user account
+    try {
+      await prisma.user.delete({
+        where: {
+          id: locals.user!.id
         }
       });
       return { form };
